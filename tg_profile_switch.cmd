c86be1bef28a02dfd40f818913910d7a7270c351
Samuel Wilson <45208386+samthelion@users.noreply.github.com>
2019-02-14 13:13:58 -0500

    Add files via upload

--- tg_profile_switch.cmd
+++ tg_profile_switch.cmd
@@ -14,17 +14,21 @@ SET/P PROFILE=# TYPE A NAME:
 IF NOT EXIST %PROFILE% GOTO BEGINING
 :CHECKRUNNING
 CLS
-FOR /F %%P IN ('tasklist /NH /FI "IMAGENAME eq %TG_EXE%"') DO IF %%P == %TG_EXE% GOTO RUNNING
 ECHO #---------------------------------#
-ECHO # REMOVING EXISTING LINK...       #
+ECHO # - CHECKING IF %TG_EXE% IS   #
+ECHO #   RUNNING...                    #
+ECHO #---------------------------------#
+FOR /F %%P IN ('tasklist /NH /FI "IMAGENAME eq %TG_EXE%"') DO IF %%P == %TG_EXE% GOTO RUNNING
+ECHO #   [%TG_EXE% ISN'T RUNNING]  #
+ECHO # - REMOVING EXISTING LINK...     #
 RMDIR /S /Q tdata
 ECHO #---------------------------------#
-ECHO # MAKING A NEW LINK TO PROFILE:   #
-ECHO # -%PROFILE%-
+ECHO # - MAKING A NEW LINK TO PROFILE: #
+ECHO #   [%PROFILE%]
 MKLINK /J tdata .\%PROFILE% >NUL
 ECHO #---------------------------------#
-ECHO # STARTING Telegram.exe...        #
-START Telegram.exe
+ECHO # - STARTING %TG_EXE%...      #
+START %TG_EXE%
 ECHO #---------------------------------#
 ECHO.
 TIMEOUT /T 5 >NUL
@@ -32,7 +36,7 @@ EXIT
 :RUNNING
 CLS
 ECHO #---------------------------------#
-ECHO # Telegram.exe IS RUNNING, CLOSE  #
+ECHO # %TG_EXE% IS RUNNING, CLOSE  #
 ECHO # THE PROGRAM TO CONTINUE...      #
 ECHO #---------------------------------#
 TIMEOUT /T 3 /NOBREAK >NUL
